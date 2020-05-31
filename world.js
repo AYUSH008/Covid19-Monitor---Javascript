e0052a7dd453509f7aa7721f8bdc50b212522c3e
Ayush <asatkar.ayush@gmail.com>
2020-05-31 13:22:29 +0530

    graph edit

--- world.js
+++ world.js
@@ -14,7 +14,7 @@ console.log(casepm);
 var worldchart1 = document.getElementById("worldchart1").getContext("2d");
 var worldchart1 = new Chart(worldchart1, {
     responsive: true,
-    type: 'line',
+    type: 'doughnut',
     data: {
         labels: ['Case per million','Recovered Per Million','active Per million' , 'Death per million'],
         datasets: [{
@@ -22,7 +22,7 @@ var worldchart1 = new Chart(worldchart1, {
             label: "Data Per Million in World",
             fill: false,
             lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
+            backgroundColor: ["rgb(230, 184, 0)" , "rgb(240, 112, 0)" , "rgb(255, 30, 5)","rgb(54, 162, 235)"] ,
             borderColor: "rgba(75,192,192,1)",
             borderCapStyle:'butt',
             borderDash :[],
